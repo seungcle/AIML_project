9099184fd519b21dd575cfba5bc74433be2b8fb2
05ovo2e <jiwonpark2229@gmail.com>
2024-11-13 23:57:38 +0900

    [Add/Update] Email Send Feature/ CORS Setting

--- receipt-app/src/main/java/com/group/receiptapp/config/MailConfig.java
+++ receipt-app/src/main/java/com/group/receiptapp/config/MailConfig.java
@@ -5,11 +5,30 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 
+import java.util.Properties;
+
 @Configuration
 public class MailConfig {
 
     @Bean
     public JavaMailSender javaMailSender() {
-        return new JavaMailSenderImpl(); // 간단한 기본 구현체
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+
+        // yml 파일의 설정을 수동으로 적용하는 경우
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(587);
+        mailSender.setUsername("hongik.receipt@gmail.com");
+        mailSender.setPassword("unmgrapiwlvsdecs");
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.ssl.enable", "false");
+        props.put("mail.smtp.connectiontimeout", "5000");
+        props.put("mail.smtp.timeout", "5000");
+        props.put("mail.smtp.writetimeout", "5000");
+
+        return mailSender;
     }
 }
---
+++ receipt-app/src/main/java/com/group/receiptapp/domain/email/EmailMessage.java
@@ -0,0 +1,16 @@
+package com.group.receiptapp.domain.email;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+public class EmailMessage {
+
+    private String to;
+    private String subject;
+    private String message;
+
+}
